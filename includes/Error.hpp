17730b9d6d8e2be576a584769dbefb867d63ce8c
Eliot Langlois <eliot.langlois6@gmail.com>
2025-09-12 01:38:07 +0200

    Add some exceptions, continue the server class init

--- includes/Error.hpp
+++ includes/Error.hpp
@@ -6,7 +6,7 @@
 /*   By: ellanglo <ellanglo@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 18:57:59 by ellanglo          #+#    #+#             */
-/*   Updated: 2025/09/11 19:05:11 by ellanglo         ###   ########.fr       */
+/*   Updated: 2025/09/12 01:24:07 by wirare           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdexcept>
@@ -24,3 +24,6 @@
 	except(std::string(msg) + "\n" + LOCATION(__FILE__, __func__, __LINE__))
 
 #define CANT_OPEN_SOCKET EXCEPTION(std::runtime_error, "Can't open socket")
+#define CANT_BIND_TO_PORT EXCEPTION(std::runtime_error, "Can't bind to port")
+#define CANT_LISTEN_SOCKET EXCEPTION(std::runtime_error, "Can't listen on socket")
+#define EPOLL_CTL_ADD_FAILURE EXCEPTION(std::runtime_error, "Error on EPOLL_CTL_ADD")
--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: ellanglo <ellanglo@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 18:37:21 by ellanglo          #+#    #+#             */
-/*   Updated: 2025/09/11 19:05:42 by ellanglo         ###   ########.fr       */
+/*   Updated: 2025/09/12 01:29:28 by wirare           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #pragma once
@@ -23,9 +23,19 @@
 class Server
 {
 public:
-	Server(int port, char *password)
+	Server(int port, char *password): port(port), password(password)
 	{
-		
+		try
+		{
+			open_socket();
+			bind_port();
+			listen_socket();
+			create_epoll();
+		}
+		catch (std::exception &e)
+		{
+			std::cout << "Error on Server setup : " << e.what() << std::endl;
+		}
 	}
 
 	void open_socket()
@@ -37,9 +47,48 @@ public:
 		setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 	}
 
+	void bind_port()
+	{
+		sockaddr_in addr;
+
+		std::memset(&addr, 0, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_addr.s_addr = INADDR_ANY;
+		addr.sin_port = htons(port);
+		socklen_t addrlen = sizeof(addr);
+		if (bind(sock_fd, reinterpret_cast<sockaddr*>(&addr), addrlen) == -1)
+		{
+			close(sock_fd);
+			throw CANT_BIND_TO_PORT;
+		}
+	}
+
+	void listen_socket()
+	{
+		if (listen(sock_fd, SOMAXCONN) == -1)
+		{
+			close(sock_fd);
+			throw CANT_LISTEN_SOCKET;
+		}
+	}
+
+	void create_epoll()
+	{
+		epoll_fd = epoll_create(1);
+
+		ev.events = EPOLLIN;
+		ev.data.fd = sock_fd;
+		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sock_fd, &ev) == -1)
+		{
+			close(sock_fd);
+			throw EPOLL_CTL_ADD_FAILURE;
+		}
+	}
+
 private:
 	int sock_fd;
 	int epoll_fd;
 	int	port;
 	char *password;
+	struct epoll_event ev, events[16];
 };
