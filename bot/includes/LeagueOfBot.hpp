6cb0a4c5e272e86ab3c84e3afd3db393b4b5266a
Joshua Douglass <jodougla@2E6.42angouleme.fr>
2025-10-01 13:35:00 +0200

    bot

---
+++ bot/includes/LeagueOfBot.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <curl/curl.h>
+
+typedef struct s_playerInfo {
+    std::string puuid;
+    std::string playerName;
+    std::string playerTag;
+    std::string region;
+}   t_playerInfo;
+
+class LeagueOfBot {
+    public :
+        LeagueOfBot();
+        ~LeagueOfBot();
+        std::string getKey() {return _apiKey;};
+        t_playerInfo getInfo() {return _Info;};
+    private :
+        t_playerInfo _Info;
+        std::string _apiKey;
+        std::map<int, std::string> _champId;
+};
